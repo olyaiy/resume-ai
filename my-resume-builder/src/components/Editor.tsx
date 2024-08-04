74a9f5ca44e2b41a18247af345ebfd43cea4ea59
olyaiy <alexolyaiy@gmail.com>
2024-08-03 22:59:11 -0700

    save button

--- my-resume-builder/src/components/Editor.tsx
+++ my-resume-builder/src/components/Editor.tsx
@@ -1,12 +1,24 @@
 'use client'
 
 import { useEditor, EditorContent } from '@tiptap/react'
-import StarterKit from '@tiptap/starter-kit'
 import Underline from '@tiptap/extension-underline'
+
+import Document from '@tiptap/extension-document'
+import Paragraph from '@tiptap/extension-paragraph'
+import Text from '@tiptap/extension-text'
+import Blockquote from '@tiptap/extension-blockquote'
+import Bold from '@tiptap/extension-bold'
+import Italic from '@tiptap/extension-italic'
+
+
+
+
+// shadcn
 import { Button } from './ui/button'
-// import { BoldIcon, ItalicIcon, UnderlineIcon } from 'lucide-react'
-import { Bold, Italic, ItalicIcon, Underline as UnderlineIconLucid } from "lucide-react"
 import { Separator } from "@/components/ui/separator"
+
+
+import { BoldIcon, ItalicIcon, TextQuote, Underline as UnderlineIconLucid } from "lucide-react"
 
 
 
@@ -14,27 +26,29 @@ import { Separator } from "@/components/ui/separator"
 
 const Editor = () => {
   const editor = useEditor({
-    
     immediatelyRender: false,
-    extensions: [StarterKit, Underline.configure({
-      HTMLAttributes: {
-        class: 'my-custom-class',
-      },
-    })   ],
+    extensions: [
+      Document,
+      Paragraph,
+      Text,
+      Bold,
+      Italic,
+      Underline,
+      Blockquote,
+    ],
+
     content: '<p>Hello World! 🌎️</p>',
      
     editorProps: {
       attributes: {
         class: 'w-full h-full focus:outline-none ',
       },},
+      injectCSS: false,
+
   
 
   })
-
   if (!editor) return <div> Editor Failed to Load</div>
-
-
-
   return (
   <div className="flex flex-col gap-4 w-full h-full ">
 
@@ -45,14 +59,13 @@ const Editor = () => {
       <Button 
       onClick={() => editor.chain().focus().toggleBold().run()} 
       className='bg-white text-black hover:bg-slate-300'> 
-      <Bold className="h-4 w-4"/>
+      <BoldIcon className="h-4 w-4"/>
       </Button>
 
       {/* Italics Button */}
       <Button 
       onClick={() => editor.chain().focus().toggleItalic().run()} 
       className={`bg-white text-black hover:bg-slate-300 ${editor.isActive('italic') ? 'is-active' : ''}`}> 
-            {/* <ItalicIcon/> */}
             <ItalicIcon className="h-4 w-4"/>
       </Button>
 
@@ -64,6 +77,22 @@ const Editor = () => {
              <UnderlineIconLucid className="h-4 w-4"/>
       </Button>
       
+      {/* Block Quote Button (doesnt work for some reason */}
+      {/* <Button
+            onClick={() => editor.chain().focus().toggleBlockquote().run()}
+                  className={editor.isActive('blockquote') ? 'is-active' : ''}> 
+             <TextQuote className="h-4 w-4"/>
+      </Button> */}
+
+      {/* Save Button */}
+      <Button 
+      onClick={() => console.log(editor.getJSON())}
+      className={`bg-green-600 text-white hover:bg-green-950`}>
+        Save
+      </Button>
+
+      
+
 
     </div>
 
