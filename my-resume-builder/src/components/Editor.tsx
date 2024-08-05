544399b3c230b1635b4f6d49be6416cadc18799c
olyaiy <alexolyaiy@gmail.com>
2024-08-05 16:20:13 -0700

    add link button component

--- my-resume-builder/src/components/Editor.tsx
+++ my-resume-builder/src/components/Editor.tsx
@@ -1,30 +1,50 @@
 'use client'
 
 import { useEditor, EditorContent } from '@tiptap/react'
-import Underline from '@tiptap/extension-underline'
 
+// Tiptap Extensions
 import Document from '@tiptap/extension-document'
 import Paragraph from '@tiptap/extension-paragraph'
+import Underline from '@tiptap/extension-underline'
 import Text from '@tiptap/extension-text'
 import Blockquote from '@tiptap/extension-blockquote'
 import Bold from '@tiptap/extension-bold'
 import Italic from '@tiptap/extension-italic'
-
-
-
+import Link from '@tiptap/extension-link'
 
 // shadcn
 import { Button } from './ui/button'
 import { Separator } from "@/components/ui/separator"
+import { Popover, PopoverContent } from './ui/popover'
+import { Input } from "@/components/ui/input"
+
+// Icons
+import { BoldIcon, ItalicIcon, LinkIcon, TextQuote, Underline as UnderlineIconLucid } from "lucide-react"
 
 
-import { BoldIcon, ItalicIcon, TextQuote, Underline as UnderlineIconLucid } from "lucide-react"
+export function InputDemo() {
+  return <Input type="email" placeholder="Email" />
+}
+import { PopoverTrigger } from '@radix-ui/react-popover'
+import { useState } from 'react'
 
+const handleSetLink = (editor: any) => {
+  // get the current input box value
+  const input = document.getElementById('link-input') as HTMLInputElement | null;
+  let url = input?.value || '';
 
+  // Add https:// if it's missing and the URL is not empty
+  if (url && !url.match(/^https?:\/\//)) {
+    url = `https://${url}`;
+  }
+  
 
+  // set the link to the correct value
+  editor.chain().focus().extendMarkRange('link').setLink({ href: url }).run();
 
-
+}
 const Editor = () => {
+
   const editor = useEditor({
     immediatelyRender: false,
     extensions: [
@@ -35,26 +55,34 @@ const Editor = () => {
       Italic,
       Underline,
       Blockquote,
-    ],
+      Link.configure({
+        protocols: ['https', 'mailto'],
+        autolink: true,
+        openOnClick: true,
+        linkOnPaste: true,
+        HTMLAttributes: {
+          class: 'text-blue-700 hover:underline cursor-pointer'
+        },
 
+      }),
+      
+    ],
     content: '<p>Hello World! 🌎️</p>',
-     
     editorProps: {
       attributes: {
         class: 'w-full h-full focus:outline-none ',
       },},
       injectCSS: false,
-
-  
-
   })
-  if (!editor) return <div> Editor Failed to Load</div>
-  return (
+
+  if (!editor) return <div> Editor Failed to Load</div>; else return (
+  
+    // Editor Container
   <div className="flex flex-col gap-4 w-full h-full ">
 
     {/* toolbar */}
-    <div 
-    className="flex flex-row gap-2 w-full rounded-lg"> 
+    <div className="flex flex-row gap-2 w-full rounded-lg"> 
+
       {/* Bold Button  */}
       <Button 
       onClick={() => editor.chain().focus().toggleBold().run()} 
@@ -77,21 +105,36 @@ const Editor = () => {
              <UnderlineIconLucid className="h-4 w-4"/>
       </Button>
       
-      {/* Block Quote Button (doesnt work for some reason */}
-      {/* <Button
-            onClick={() => editor.chain().focus().toggleBlockquote().run()}
-                  className={editor.isActive('blockquote') ? 'is-active' : ''}> 
-             <TextQuote className="h-4 w-4"/>
-      </Button> */}
+      {/* Link Button */}
+
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button 
+              className={`bg-white text-black hover:bg-slate-300 ${editor.isActive('underline') ? 'is-active' : ''}`}>
+            <LinkIcon className="h-4 w-4"/>
+          </Button>
+        </PopoverTrigger>
+
+        <PopoverContent className='w-full p-4 flex flex-col gap-4'>
+          <div className="flex flex-row gap-2">
+            <Input className="w-64" id="link-input"/>
+            <Button onClick={() => handleSetLink(editor)}>  Add Link </Button>
+          </div>
+
+          <Button
+            onClick={() => editor.chain().focus().unsetLink().run()}
+          > Remove Link</Button>
+        </PopoverContent>
+      </Popover>
+
+
 
       {/* Save Button */}
-      <Button 
-      onClick={() => console.log(editor.getJSON())}
-      className={`bg-green-600 text-white hover:bg-green-950`}>
-        Save
-      </Button>
-
-      
+        <Button 
+        onClick={() => console.log(editor.getJSON())}
+        className={`bg-green-600 text-white hover:bg-green-950`}>
+          Save
+        </Button>
 
 
     </div>
