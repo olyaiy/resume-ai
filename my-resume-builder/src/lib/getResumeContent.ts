40cc2e687bc4472691d186b0c889100d72a8d7dc
olyaiy <alexolyaiy@gmail.com>
2024-08-25 17:05:39 -0700

    restart project to re-build based on new items learned

--- my-resume-builder/src/lib/getResumeContent.ts
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-import path from 'path';
-
-export function getResumeContent() {
-  const filePath = path.join(process.cwd(), 'src', 'resumes', 'resume.json');
-  const fileContent = fs.readFileSync(filePath, 'utf8');
-  return JSON.parse(fileContent);
-}
