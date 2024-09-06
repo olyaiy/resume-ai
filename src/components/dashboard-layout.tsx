46b1bf37ec99eb0ad46f96514689a91260bbee0a
olyaiy <alexolyaiy@gmail.com>
2024-09-06 15:22:29 -0700

    add sidebar prototype

--- src/components/dashboard-layout.tsx
+++ src/components/dashboard-layout.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import { Button } from './ui/button';
 
 
 
@@ -8,7 +9,16 @@ interface DashboardLayoutProps {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
-    <div>
+    <div className="h-screen w-screen flex flex-row">
+      <div className="flex h-full w-64 flex-col justify-center bg-secondary border-r-2 border-border">
+
+        <div className="flex shrink-0 items-center flex-col">
+          <Button variant="ghost">Link 1</Button>
+          <Button variant="ghost">Link 2</Button>
+          <Button variant="ghost">Link 3</Button>
+          <Button variant="ghost">Link 4</Button>
+        </div>
+      </div>
       {children}
     </div>
   );
