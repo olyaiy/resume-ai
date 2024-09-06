f857b6dac3dfee152483d368d8fada4573c961b0
olyaiy <alexolyaiy@gmail.com>
2024-09-06 15:25:57 -0700

    add links to sidebar

--- src/components/dashboard-layout.tsx
+++ src/components/dashboard-layout.tsx
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react';
 import { Button } from './ui/button';
-
-
+import Link from 'next/link';
 
 interface DashboardLayoutProps {
   children: ReactNode;
@@ -11,12 +10,22 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="h-screen w-screen flex flex-row">
       <div className="flex h-full w-64 flex-col justify-center bg-secondary border-r-2 border-border">
-
         <div className="flex shrink-0 items-center flex-col">
-          <Button variant="ghost">Link 1</Button>
-          <Button variant="ghost">Link 2</Button>
-          <Button variant="ghost">Link 3</Button>
-          <Button variant="ghost">Link 4</Button>
+          <Link href="/dashboard" passHref>
+            <Button variant="ghost" className="w-full">Dashboard</Button>
+          </Link>
+          <Link href="/editor" passHref>
+            <Button variant="ghost" className="w-full">New Resume</Button>
+          </Link>
+          <Link href="/resumes" passHref>
+            <Button variant="ghost" className="w-full">Resumes</Button>
+          </Link>
+          <Link href="/profile" passHref>
+            <Button variant="ghost" className="w-full">Profile</Button>
+          </Link>
+          <Link href="/settings" passHref>
+            <Button variant="ghost" className="w-full">Settings</Button>
+          </Link>
         </div>
       </div>
       {children}
