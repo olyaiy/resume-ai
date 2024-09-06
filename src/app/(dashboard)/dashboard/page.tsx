b3b3910d1813d70164646c2ec15789ec9bacdac1
olyaiy <alexolyaiy@gmail.com>
2024-09-06 14:53:31 -0700

    set up project structure

---
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div>
+            Hello this is dashboard
+        </div>
+    );
+}
---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,17 @@
+import DashboardLayout from '@/components/dashboard-layout';
+import { ReactNode } from 'react';
+
+
+export default function Layout({
+  children,
+}: {
+  children: ReactNode
+}) {
+  return (
+
+      <DashboardLayout>
+      {children}
+      </DashboardLayout>
+
+  );
+}
