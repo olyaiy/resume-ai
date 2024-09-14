c85c85e61be07294baa590fbfa9d7772fa082cc0
olyaiy <alexolyaiy@gmail.com>
2024-09-13 19:50:43 -0700

    remove sidebar and add links to navbar

--- src/components/dashboard-layout.tsx
+++ src/components/dashboard-layout.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
 
-import SideBar from './side-bar';
+
 import NavBar from './nav-bar';
 
 interface DashboardLayoutProps {
@@ -11,8 +11,8 @@ interface DashboardLayoutProps {
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div className="w-screen flex flex-row h-screen">
-      <SideBar />
-      <div className="w-full m-4 rounded-md bg-secondary ml-[17rem] mt-[4.5rem] p-4 overflow-scroll">
+
+      <div className="w-full m-4 rounded-md bg-secondary mt-[4.5rem] p-4 overflow-scroll">
         {children}
       </div>
     </div>
