f54703e9f0141ab55670316e7aa82ccf42024591
olyaiy <alexolyaiy@gmail.com>
2024-09-21 15:14:21 -0700

    add redirection logic to sign-up page which will lead user to dashboard home if they are logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,27 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { LoginForm } from "@/components/auth/login-form"
 import SignUpForm from "@/components/auth/signup-form"
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { isTokenExpired } from 'pocketbase'
+
 
 export default function Page() {
+
+  // If the user is already logged in, redirect to the dashboard
+  const cookieStore = cookies()
+  const authCookie = cookieStore.get('pb_auth')
+  
+  if (authCookie) {
+    const token = JSON.parse(authCookie.value).token
+    if (token && !isTokenExpired(token)) {
+      redirect('/dashboard')
+    }
+  }
+
+
+  
+
   return (
     <div className="w-full h-screen flex items-center justify-center flex-col">
       <Tabs defaultValue="signin" className="w-full max-w-sm">
