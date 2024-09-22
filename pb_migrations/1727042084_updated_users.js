2267e7f17dedd2baf87c1223ee52a39cc411967c
olyaiy <alexolyaiy@gmail.com>
2024-09-22 15:08:05 -0700

    update work experience section ui and data type for profile

---
+++ pb_migrations/1727042084_updated_users.js
@@ -0,0 +1,48 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("zokoyluw")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "5jx92xoz",
+    "name": "skills",
+    "type": "text",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "min": null,
+      "max": null,
+      "pattern": ""
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "zokoyluw",
+    "name": "skills",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 2000000
+    }
+  }))
+
+  // remove
+  collection.schema.removeField("5jx92xoz")
+
+  return dao.saveCollection(collection)
+})
--- src/components/profile/profile-skills.tsx
+++ src/components/profile/profile-skills.tsx
@@ -14,8 +14,15 @@ const ProfileSkills = ({ profile, setProfile }: { profile: UserProfile, setProfi
         id="skills"
         value={profile.skills}
         onChange={(e) => handleSkillsChange(e.target.value)}
-        className="w-full"
-        placeholder="Enter your skills (comma-separated)"
+        className="w-full h-72"
+        placeholder={`Example:
+Languages: Python, JavaScript, Typescript, C, C++, Java, HTML, CSS, SQL, BASH
+
+Frameworks/Libraries: React, ReactNative, NextJS, Express.js, Node.js, Mongoose, GraphQL, Tailwind, Vite
+
+Developer Tools: Git, Docker, AWS (S3, EC2, ECS, Lambda), Linux, MSSQL, PostgreSQL, NGINX, REST APIs
+
+Other Skills: Web development, Data processing, Data handling, Data analysis, Database management, Content management systems, Workflow creation`}
         rows={5}
       />
     </div>
