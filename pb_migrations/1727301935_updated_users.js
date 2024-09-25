4f8f79d94c34a63e526cfe44e1225f26e1395127
olyaiy <alexolyaiy@gmail.com>
2024-09-25 15:25:43 -0700

    minor fixes

---
+++ pb_migrations/1727301935_updated_users.js
@@ -0,0 +1,91 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "8dztfghh",
+    "name": "formatted_skills",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 2000000
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "trhhhpop",
+    "name": "formatted_work_history",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 2000000
+    }
+  }))
+
+  // add
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "vvqg5ylb",
+    "name": "formatted_projects",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 2000000
+    }
+  }))
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "htuid86p",
+    "name": "prindex8groupdefaultojects",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 2000000
+    }
+  }))
+
+  return dao.saveCollection(collection)
+}, (db) => {
+  const dao = new Dao(db)
+  const collection = dao.findCollectionByNameOrId("_pb_users_auth_")
+
+  // remove
+  collection.schema.removeField("8dztfghh")
+
+  // remove
+  collection.schema.removeField("trhhhpop")
+
+  // remove
+  collection.schema.removeField("vvqg5ylb")
+
+  // update
+  collection.schema.addField(new SchemaField({
+    "system": false,
+    "id": "htuid86p",
+    "name": "projects",
+    "type": "json",
+    "required": false,
+    "presentable": false,
+    "unique": false,
+    "options": {
+      "maxSize": 2000000
+    }
+  }))
+
+  return dao.saveCollection(collection)
+})
